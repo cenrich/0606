225bd96fec05508969aef926e4263552a3851e53
cenrich <ceci_enrich@hotmail.com>
2019-06-14 20:22:39 -0300

    un elemento se borra

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let order = []
+let orderList = []
 const menu = [
-    { id:0001, name: "Tarta de jamón y queso", type: "principal", price:100, promo:"1" },
-	{ id:0002, name: "Ensalada caprese" , type: "principal", price:100, promo:"1" },
-	{ id:0003, name: "Milanesa" , type: "principal", price:100, promo:"1" },
-	{ id:0004, name: "Ensalada mixta", type: "guarnicion", price:100, promo:"1" },
-	{ id:0005, name: "Papas fritas", type: "guarnicion", price:100, promo:"1" },
-	{ id:0006, name: "Puré de zapallo", type: "guarnicion", price:100, promo:"1" },
-	{ id:0007, name: "Flan con crema", type: "postre", price:100, promo:"1" },
-	{ id:0008, name: "Queso y dulce", type: "postre", price:100, promo:"1" },
-    { id:0009, name: "Mousse de chocolate", type: "postre", price:100, promo:"1" },
-    { id:0010, name: "Coca Cola", type: "bebida", price:100, promo:"1" },
-    { id:0011, name: "Fanta", type: "bebida", price:100, promo:"1" },
+    { id:'0001', name: "Tarta de jamón y queso", type: "principal", price:100, promo:"1" },
+	{ id:'0002', name: "Ensalada caprese" , type: "principal", price:100, promo:"2" },
+	{ id:'0003', name: "Milanesa" , type: "principal", price:100, promo:"3" },
+	{ id:'0004', name: "Ensalada mixta", type: "guarnicion", price:100, promo:"1" },
+	{ id:'0005', name: "Papas fritas", type: "guarnicion", price:100, promo:"2" },
+	{ id:'0006', name: "Puré de zapallo", type: "guarnicion", price:100, promo:"3" },
+	{ id:'0007', name: "Flan con crema", type: "postre", price:100, promo:"1" },
+	{ id:'0008', name: "Queso y dulce", type: "postre", price:100, promo:"2" },
+    { id:'0009', name: "Mousse de chocolate", type: "postre", price:100, promo:"3" },
+    { id:'0010', name: "Coca Cola", type: "bebida", price:100, promo:"1" },
+    { id:'0011', name: "Fanta", type: "bebida", price:100, promo:"2" },
+    { id:'0012', name: "Fanta Light", type: "bebida", price:100, promo:"3" }
 ]
 
 element = {
-    id:0012,
+    id:'0013',
     name:'Paso de los Toros',
     type:'Bebida',
     price:50,
@@ -57,7 +58,7 @@ const createOption = elem => {
 }
 
 
-//4. una función que ponga en cada select una opción vacía como placeholder.
+//4. una función que ponga en cada select una opción vacía como placeholder. esta no se usó
 const emptyOption = list => {
     list.forEach (e=>{
         let select = document.getElementById(e)
@@ -67,10 +68,88 @@ const emptyOption = list => {
     })
 }
 
+
+var createButton=function(classBtn, index, btnFunction){
+    btn=document.createElement('button')
+    btn.classList.add(classBtn)
+    btn.innerText=classBtn
+    btn.id=index
+    btn.onclick=function(){btnFunction(this)}
+    return btn
+}
+
+var done = function (btn) {
+    orderList.splice (btn.id,1)
+    printOrders()
+}
+
+const printOrders=() => {
+    let container =document.getElementById("orders")
+    container.innerHTML=""
+    orderList.forEach ((order,index) => {
+        let orderUl= document.createElement("ul")
+        order.forEach(plate=>{
+            let plateLi=document.createElement("li")
+            plateLi.innerText=plate.name
+            orderUl.appendChild(plateLi)
+        })
+        let priceLi =document.createElement("li")
+        priceLi.innerText =totalPrice(order)
+        orderUl.appendChild(priceLi)
+        orderUl.appendChild(createButton('done', index,done))
+        container.appendChild(orderUl)
+    })
+}
+
+// const totalPrice =order=> {
+//     let total = 0
+//     order.forEach(plate=>total+=plate.price)
+//     return total
+// }
+// atención pues acá puede hacerse de otra manera con list.reduce. (ver a continuación)
+
+// const totalPrice =order=>order.map((e=>e.price)).reduce((a,b)=>a+b)// ver esto para el array de precios
+
+//ahora si todos coinciden con lapromo, hay un descuente 
+const totalPrice= order=> {
+    let price=order.map((e=>e.price)).reduce((a,b)=>a+b)
+    let promo=order.map(e=>e.promo).reduce((a,b)=>a===b? a:undefined)
+    switch (promo) {
+        case "1":
+        return price*.8
+        break
+        case "2":
+        return price*.75
+        break
+        case "3":
+        return price*.85
+        break
+        default:
+            return price
+    }
+    return price
+}
+
+
 //inicialización del programa
 const initialize = () => {
     let mainDiv = document.getElementById("selects")
     createSelects (plateTypes, mainDiv)
     // emptyOption(plateTypes)
     fillSelects(menu)
+    printOrders()
+}
+
+//Quiero confirmar el menú
+const createOrder =() => {
+    let order=[]
+    plateTypes.forEach (e=> {
+        let select =document.getElementById(e)
+        // acá falta algo
+        let plate = menu.find(e=>e.id===select.value)
+        order.push(plate)
+        select.value=""
+    })
+    orderList.push(order)
+    printOrders()
 }
